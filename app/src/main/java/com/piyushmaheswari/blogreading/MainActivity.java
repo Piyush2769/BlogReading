b5df4db67997d4027223702b1eeaba87e1ed3670
Piyush2769 <startcheckingyourmail@gmail.com>
2019-09-15 23:58:07 +0530

    Initial +1

--- app/src/main/java/com/piyushmaheswari/blogreading/MainActivity.java
+++ app/src/main/java/com/piyushmaheswari/blogreading/MainActivity.java
@@ -1,9 +1,23 @@
 package com.piyushmaheswari.blogreading;
 
 import android.os.AsyncTask;
+import android.provider.DocumentsContract;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import java.io.InputStream;
+import java.lang.annotation.Documented;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,21 +26,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        PlaceHolderFragment taskFragment;
+
         if(savedInstanceState==null)
         {
-
+            taskFragment=new PlaceHolderFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .add(taskFragment,"MyFragment").commit();
         }
         else
         {
+            taskFragment=(PlaceHolderFragment) getSupportFragmentManager()
+                                .findFragmentByTag("MyFragment");
 
         }
+        taskFragment.startTask();
     }
 
     public static class PlaceHolderFragment extends Fragment{
 
+        TechCrunchTask downloadadTask;
         public PlaceHolderFragment()
         {
 
+        }
+
+        @Override
+        public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+            super.onActivityCreated(savedInstanceState);
+            setRetainInstance(true);
+        }
+
+        public void startTask()
+        {
+            if(downloadadTask!=null)
+            {
+                downloadadTask.cancel(true);
+            }
+            else {
+                downloadadTask=new TechCrunchTask();
+                downloadadTask.execute();
+            }
         }
     }
 
@@ -35,7 +75,32 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected Void doInBackground(Void... voids) {
+
+            String downloadUrl="http://feeds.feedburner.com/techcrunch/android?format=xml";
+            try {
+                URL url=new URL(downloadUrl);
+                HttpURLConnection connection=(HttpURLConnection)url
+                        .openConnection();
+                connection.setRequestMethod("GET");
+                InputStream inputStream=connection.getInputStream();
+                processXML(inputStream);
+
+
+            } catch (Exception e) {
+
+            }
+
             return null;
+        }
+
+        public void processXML(InputStream inputStream) throws Exception{
+
+            DocumentBuilderFactory documentBuilderFactory=DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder=documentBuilderFactory.newDocumentBuilder();
+            Document xmlDocument=documentBuilder.parse(inputStream);
+
+            Element rootElement=xmlDocument.getDocumentElement();
+
         }
     }
 }
